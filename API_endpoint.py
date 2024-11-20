c723c2d437afd0cacefec1ae2c74bacef7600772
Christian Cruz <71656856+AtomicHunters@users.noreply.github.com>
2024-11-19 21:58:32 -0600

    Add files via upload
    This should take the image and return an embedding for now. Will need to get it working with the database.

---
+++ API_endpoint.py
@@ -0,0 +1,45 @@
+from flask import Flask, request, jsonify
+import os
+from PIL import Image
+from models import get_clip_model
+
+app = Flask(__name__)
+
+#These set up a folder to store the uploaded files
+UPLOAD_FOLDER = './uploads'
+os.makedirs(UPLOAD_FOLDER, exist_ok= True)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+#Set the types of images that are allowed
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/upload', methods =['POST'])
+def upload_image():
+    # Check if the request contains a file
+    if 'image' not in request.files:
+        return jsonify({'error': 'No image file in request'}), 400
+
+    # Get the image and make sure the file is the correct type
+    image_file = request.files['image']
+    if not allowed_file(image_file.filename):
+        return jsonify({'error': 'Invalid file type'}), 400
+
+    # Save the file
+    try:
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], image_file.filename)
+        image_file.save(file_path)
+
+    except Exception as e:
+        return jsonify({'error':f'Failed to save file: {str(e)}'}),500
+
+    # Set the image to the filepath for the saved image
+    image = Image.open(image_file.filename)
+
+    # Now embed the images using the image
+    embedding = get_clip_model(image)
+
+    # Return the embedding for testing. Will need to instead send this to the database and return what is given from there.
+    return embedding
+
