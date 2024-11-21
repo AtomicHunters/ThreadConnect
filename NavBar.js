9dd9fd35007677d0d4c774c1a165fbc20795c5cf
veronikatunyk <verontunyk@gmail.com>
2024-11-20 18:23:06 -0600

    Update NavBar.js
    fixed comments

--- NavBar.js
+++ NavBar.js
@@ -1,12 +1,12 @@
 import { Nav, NavLink, NavMenu } from "./NavbarElements";
 
 const Navbar = () => {
+    /* Navigation links - Home (defaault), and image Upload page */
     return (
 
 
         <Nav>
             <NavMenu>
-                /* Navigation links - Home (defaault), and image Upload page */
                 <NavLink to="/">Home</NavLink>
                 <NavLink to="/image_upload">Image Upload</NavLink>
             </NavMenu>
